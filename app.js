c3c0e5da6dcbe881954977f66d63d55887a21d6f
lean9ema <lean9marino@gmail.com>
2021-12-03 15:47:15 -0300

    Lean-agregue rutas hacia product

--- app.js
+++ app.js
@@ -8,8 +8,9 @@ app.set('view engine','ejs'); // Para "activar" ejs
 app.set('views',__dirname + '/src/views'); // No hace falta, cuando se tiene a views en la parte principal 
 
 const usersRoutes = require('./src/routers/userRoutes');
-
+const productRoutes = require('./src/routers/productRoutes');
 app.use('/users', usersRoutes);
+app.use('/product', usersRoutes);
 
 app.listen (port, () =>{
     console.log(`Servidor funcionando en el puerto ${port}` )
