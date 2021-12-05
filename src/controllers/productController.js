6b697ea1c71c2a1eb2c01f7754801334d4f8174a
lean9ema <lean9marino@gmail.com>
2021-12-04 22:12:12 -0300

    Lean-detalles en ejs para almacenar y mostrar mediante arrays datos de los productos

--- src/controllers/productController.js
+++ src/controllers/productController.js
@@ -2,50 +2,62 @@ let listaProducts = [
     {
         id:1,
         nombre:'Remera blanca con estampado',
-        precio: '$100.000',
+        precio: '100.000',
         descripcion:'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Excepturi nemo ipsa amet quaerat sed rem veritatis, cumque eaque natus aperiam, perferendis quibusdam neque harum itaque consequuntur dicta a voluptate maxime',
         img: 'img-ropa.jpeg',
-        categoria: 'ropa'
+        categoria: 'ropa',
+        colores: ['red','black','olive','blue','goldenrod'],
+        talles: ['XS','S','M','L','XL']
     },
     {
         id:2,
         nombre:'Remera blanca con estampado',
-        precio: '$100.000',
+        precio: '100.000',
         descripcion:'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Excepturi nemo ipsa amet quaerat sed rem veritatis, cumque eaque natus aperiam, perferendis quibusdam neque harum itaque consequuntur dicta a voluptate maxime',
         img: 'img-ropa.jpeg',
-        categoria: 'ropa'
+        categoria: 'ropa',
+        colores: ['red','black','olive','blue','goldenrod'],
+        talles: ['XS','S','M','L','XL']
     },
     {
         id:3,
         nombre:'Remera blanca con estampado',
-        precio: '$100.000',
+        precio: '100.000',
         descripcion:'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Excepturi nemo ipsa amet quaerat sed rem veritatis, cumque eaque natus aperiam, perferendis quibusdam neque harum itaque consequuntur dicta a voluptate maxime',
         img: 'img-ropa.jpeg',
-        categoria: 'ropa'
+        categoria: 'ropa',
+        colores: ['red','black','olive','blue','goldenrod'],
+        talles: ['XS','S','M','L','XL']
     },
     {
         id:4,
         nombre:'Remera blanca con estampado',
-        precio: '$100.000',
+        precio: '100.000',
         descripcion:'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Excepturi nemo ipsa amet quaerat sed rem veritatis, cumque eaque natus aperiam, perferendis quibusdam neque harum itaque consequuntur dicta a voluptate maxime',
         img: 'img-ropa.jpeg',
-        categoria: 'ropa'
+        categoria: 'ropa',
+        colores: ['red','black','olive','blue','goldenrod'],
+        talles: ['XS','S','M','L','XL']
     },
     {
         id:5,
         nombre:'Remera blanca con estampado',
-        precio: '$100.000',
+        precio: '100.000',
         descripcion:'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Excepturi nemo ipsa amet quaerat sed rem veritatis, cumque eaque natus aperiam, perferendis quibusdam neque harum itaque consequuntur dicta a voluptate maxime',
         img: 'img-ropa.jpeg',
-        categoria: 'ropa'
+        categoria: 'ropa',
+        colores: ['red','black','olive','blue','goldenrod'],
+        talles: ['XS','S','M','L','XL']
     },
     {
         id:6,
         nombre:'Remera blanca con estampado',
-        precio: '$100.000',
+        precio: '100.000',
         descripcion:'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Excepturi nemo ipsa amet quaerat sed rem veritatis, cumque eaque natus aperiam, perferendis quibusdam neque harum itaque consequuntur dicta a voluptate maxime',
         img: 'img-ropa.jpeg',
-        categoria: 'ropa'
+        categoria: 'ropa',
+        colores: ['red','black','olive','blue','goldenrod'],
+        talles: ['XS','S','M','L','XL']
     }
 ]
 
