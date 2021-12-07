a37969f8b79b1841e3f1d7632bc385ef5620f56b
lean9ema <lean9marino@gmail.com>
2021-12-07 11:29:38 -0300

    lean-

--- src/routers/productRoutes.js
+++ src/routers/productRoutes.js
@@ -4,11 +4,8 @@ const router = express.Router();
 
 // Acá definimos las rutas
 router.get('/productList',productController.list)
-<<<<<<< HEAD
 router.get('/charger',productController.charger)
-=======
 router.get('/productDetail/:productId', productController.prodDetail );
 
->>>>>>> a92a023edb1057155ae82d73db3f86dd1a209f2e
 
 module.exports = router;
